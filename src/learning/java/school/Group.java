ceca7f7e10bdd75dfd687290b25c9f7c339036cc
HectorHernandez19 <hectorgustavo150@hotmail.com>
2020-09-10 03:48:53 -0500

    Tarea

---
+++ src/learning/java/school/Group.java
@@ -0,0 +1,14 @@
+package learning.java.school;
+
+class Group {
+    private Professor professor;
+    private int availability;
+    private Student[] students;
+
+    Group(int availability) {
+        this.availability = availability;
+        students = new Student[availability];
+    }
+
+}
+
